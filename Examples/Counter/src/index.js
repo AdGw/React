1d68d701cc270eee602f93d793b579121a332f92
AdGw <adrian.gwozdziej@gmail.com>
2018-09-24 10:11:33 +0200

    Add counting app

--- Examples/Counter/src/index.js
+++ Examples/Counter/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+let createReactClass = require('create-react-class');
+let Product = createReactClass({
+    getInitialState: ()=>{
+        return {qty: 0}
+    },
+    buy: function(){
+        this.setState({
+            qty: this.state.qty +1
+        });
+    },
+    render: function(){
+        return(
+            <div>
+                <p>Game - $50</p>
+                <button onClick={this.buy}>Buy</button>
+                <h4>Qty: {this.state.qty} item(s)</h4>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<Product />, document.getElementById('root'));
+
