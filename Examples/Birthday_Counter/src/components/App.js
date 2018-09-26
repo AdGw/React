e465b081e1220dc5ddea039b9f778a82699eddd5
AdGw <adrian.gwozdziej@gmail.com>
2018-09-26 13:07:01 +0200

    Add validation form, events

--- Examples/Birthday_Counter/src/components/App.js
+++ Examples/Birthday_Counter/src/components/App.js
@@ -1,8 +1,36 @@
 import React, {Component} from 'react';
+import {Form, FormControl, Button} from 'react-bootstrap'
+import './App.css'
 class App extends Component{
+
+    constructor(){
+        super();
+        this.state={
+            newDate: '',
+            birthday: '1993-07-01'
+        }
+    }
+    changeBirthday(){
+        console.log(this.state)
+        this.setState({
+            birthday: this.state.newDate
+        });
+    }
     render(){
         return(
-            <div>Component</div>
+            <div className = "App">
+                <h2>Input Your Birthday</h2>
+                <Form inline className = "form">
+                    <FormControl type = "date" onChange={event=>this.setState({
+                        newDate: event.target.value
+                    })}>
+                    </FormControl>
+                    { ' ' }
+                    <Button onClick={()=>this.changeBirthday()}>
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         )
     }
 }
