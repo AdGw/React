6aa623ca8647c7db951e319495fad2087729b2d7
AdGw <adrian.gwozdziej@gmail.com>
2018-09-29 14:49:27 +0200

    add events and set state

--- Recipe_Finder/src/components/SearchRecipes.js
+++ Recipe_Finder/src/components/SearchRecipes.js
@@ -2,29 +2,53 @@ import React, { Component } from 'react'
 import {Form, FormGroup, ControlLabel, Button, FormControl} from 'react-bootstrap'
 
 class SearchRecipes extends Component {
-  render() {
-    return (
-        <Form>
-            <FormGroup>
-                <ControlLabel>
-                    Ingrediens
-                </ControlLabel><br/>
+    constructor(){
+        super();
+        this.state={
+            ingrediens: '',
+            dish: ''
+        }
+    }
+    search(state){
+        let {
+            ingrediens, dish
+        }=this.state;
+        const url = `http://www.recipepuppy.com/api/?i=${ingrediens}&q=${dish}`;
+    }
+
+    render() {
+        return (
+            <Form>
+                <FormGroup>
+                    <ControlLabel>
+                        Ingrediens
+                    </ControlLabel><br/>
+                    { " " }
+                    <FormControl 
+                    className = "form-control" type="text" 
+                    placeholder = "garlic, chicken"
+                    onChange={event=>this.setState({
+                        ingrediens: event.target.value
+                    })}/>
+                </FormGroup>
                 { " " }
-                <FormControl className = "form-control" type="text" placeholder = "garlic, chicken"/>
-            </FormGroup>
-            { " " }
-            <FormGroup>
-                <ControlLabel>
-                    { ' ' }
-                    Dish
-                </ControlLabel><br/>
-                <FormControl className = "form-control" type="text" placeholder = "adobo"/>
-            </FormGroup>
-            { ' ' }
-            <Button>Submit</Button>
-        </Form>
-    )
-  }
+                <FormGroup>
+                    <ControlLabel>
+                        { ' ' }
+                        Dish
+                    </ControlLabel><br/>
+                    <FormControl 
+                    className = "form-control" type="text" 
+                    placeholder = "adobo"
+                    onChange={event=>this.setState({
+                        dish: event.target.value
+                    })}/>
+                </FormGroup>
+                { ' ' }
+                <Button onClick={()=>this.search()}>Submit</Button>
+            </Form>
+        )
+    }
 }
 
 export default SearchRecipes;
