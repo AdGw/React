af1d04aec8e1b0f0c76e0a64274d564147dfcb3b
AdGw <adrian.gwozdziej@gmail.com>
2018-10-01 15:41:04 +0200

    Listing items, stylize

--- Meme_Generator/src/components/App.js
+++ Meme_Generator/src/components/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import '../styles/style.css'
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      memeLimit: 10
+    }
+  }
   render() {
     return (
       <div>
-        
+        {
+          this.props.memes.slice(0,this.state.memeLimit).map((meme, index)=>{
+            return(
+              <h4 key = {index}>{meme.name}</h4>
+            )
+          })
+        }
+        <div className="meme-button" onClick = {()=>{
+          this.setState({memeLimit: this.state.memeLimit+10
+          })
+        }}>
+        Load More ...</div>
       </div>
     )
   }
 }
 
-export default App;
+function mapStateToProps(state){
+  return state;
+}
+
+export default connect(mapStateToProps, null)(App);
