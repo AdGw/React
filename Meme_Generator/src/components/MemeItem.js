6be12a376a57a4427e1f5a32a73a9323d9e24a0d
AdGw <adrian.gwozdziej@gmail.com>
2018-10-01 18:37:44 +0200

    add post method

--- Meme_Generator/src/components/MemeItem.js
+++ Meme_Generator/src/components/MemeItem.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {createMeme} from '../actions'
 
 class MemeItem extends Component {
     constructor(){
@@ -7,24 +9,37 @@ class MemeItem extends Component {
             hovered: false
         }
     }
-  render() {
+
+    postMeme(){
+        const {text0, text1} = this.props;
+        const memeObj = {
+            template_id: this.props.meme.id,
+            text0,
+            text1
+        }
+        this.props.createMeme(memeObj);
+    }
+
+    render() {
     return (
-      <div className = "meme-item" 
+        <div className = "meme-item" 
         onMouseEnter={()=> this.setState({
             hovered: true
         })} 
         onMouseLeave={()=> this.setState({
             hovered: false
-        })}>
+        })}
+        onClick={()=> this.postMeme()}
+        >
         <img className={this.state.hovered ? "meme-img darken-img" : "meme-img"} src = {this.props.meme.url}
         alt = {this.props.meme.name}
         />
         <p className={this.state.hovered ? "meme-text" : "no-text"}>
             {this.props.meme.name}
         </p>
-      </div>
-    )
-  }
+        </div>
+        )
+    }
 }
 
-export default MemeItem;
+export default connect(null, {createMeme})(MemeItem);
