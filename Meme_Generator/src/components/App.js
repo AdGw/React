40a28a635eacb93cc717b33c3e2e6867b1f09751
AdGw <adrian.gwozdziej@gmail.com>
2018-10-01 17:00:22 +0200

    add validation forms with event logic

--- Meme_Generator/src/components/App.js
+++ Meme_Generator/src/components/App.js
@@ -2,17 +2,45 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import '../styles/style.css'
 import MemeItem from './MemeItem'
+import {Form, FormGroup, FormControl, ControlLabel} from 'react-bootstrap'
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      memeLimit: 10
+      memeLimit: 10,
+      text0: '',
+      text1: ''
     }
   }
   render() {
     return (
       <div>
+        <h4><i>Write some text</i></h4>
+        <Form>
+          <FormGroup>
+            <ControlLabel >
+              Top
+            </ControlLabel>
+            {' '}
+            <FormControl type = "text" onChange={event=> this.setState({
+              text0: event.target.value
+            })}>
+
+            </FormControl>
+          </FormGroup>
+          {' '}
+          <FormGroup>
+            <ControlLabel>
+              Bottom
+            </ControlLabel>
+            {' '}
+            <FormControl type = "text" onChange={event=> this.setState({
+              text1: event.target.value
+            })}>
+            </FormControl>
+          </FormGroup>
+        </Form>
         {
           this.props.memes.slice(0,this.state.memeLimit).map((meme, index)=>{
             return(
