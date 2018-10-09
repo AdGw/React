d9c395104510b91126be36dd51d3d02d8cffcdd1
Adgw <adrian.gwozdziej@gmail.com>
2018-10-09 19:09:39 +0200

    Add hover options to display height of bars

--- ReactJS/Chart/src/component/Chart.js
+++ ReactJS/Chart/src/component/Chart.js
@@ -45,6 +45,14 @@ class Chart extends Component {
 
     render() {
         const chart = ReactFauxDOM.createElement('div');
+        let tooltip = d3.select('body').append('div')
+            .style('position', 'absolute')
+            .style('background', '#ccc')
+            .style('opacity', '0')
+            .style('padding', '10px')
+            .style('border', '1px #000000 solid')
+            .style('border-radius', '5px')
+
         d3.select(chart).append('svg')
             .attr('width', this.props.width)
             .attr('height', this.props.height)
@@ -56,7 +64,17 @@ class Chart extends Component {
                 .attr('width', this.props.barWidth)
                 .attr('height', (d)=>this.state.yScale(d))
                 .attr('x', (d, i)=> this.state.xScale(i))
-                .attr('y', (d)=>this.props.height - this.state.yScale(d));
+                .attr('y', (d)=>this.props.height - this.state.yScale(d))
+
+                .on('mouseover', (d)=>{
+                    tooltip.style('opacity', 1)
+                    tooltip.html(d)
+                        .style('left',(d3.event.pageX)+'px')
+                        .style('top',(d3.event.pageY)+'px')
+                })
+                .on('mouseout', (d)=>{
+                    tooltip.style('opacity', 0);
+                })
         return chart.toReact();
     }
 }
