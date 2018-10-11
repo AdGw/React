eb400f91d83432b7c8b30dd4e2ff87cfd0b65deb
AdGw <adrian.gwozdziej@gmail.com>
2018-10-11 12:28:18 +0200

    display data from json

---
+++ idle/src/components/HeroList.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Characters from '../data/Characters.json'
+import {Grid, Row, Col} from 'react-bootstrap';
+import '../App.css'
+
+class HeroList extends Component {
+  render() {
+    return (
+      <div>
+        {Characters.map((postDetail, index)=>{
+            return(
+            <Grid className = "hero-list">
+                <Row >
+                    <Col xs = {2} sm={2} md={2}> 
+                        <div>
+                            <h5>{postDetail.name}</h5>
+                        </div>
+                    </Col>
+                    <Col xs = {2} sm={2} md={2}> 
+                        <div>
+                            <h5>{postDetail.level}</h5>
+                        </div>
+                    </Col>
+                    <Col xs = {2} sm={2} md={2}> 
+                        <div>
+                            <h5>{postDetail.hp}</h5>
+                        </div>
+                    </Col>
+                    <Col xs = {2} sm={2} md={2}> 
+                        <div>
+                            <h5>{postDetail.mana}</h5>
+                        </div>
+                    </Col>
+                    <Col xs = {2} sm={2} md={2}> 
+                        <div>
+                            <h5>{postDetail.dmg}</h5>
+                        </div>
+                    </Col>
+                </Row>
+            </Grid>
+            )
+        })}
+      </div>
+    );
+  }
+}
+
+export default HeroList;
--- idle/src/components/Layout.js
+++ idle/src/components/Layout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Grid, Row, Col} from 'react-bootstrap';
+import HeroList from './HeroList'
 import '../App.css'
 
 class Layout extends Component {
@@ -9,46 +10,31 @@ class Layout extends Component {
       <div className="layout">
         <Grid className="show-grid layout">
             <Row >
-            <Col xs = {2} sm={2} md={2}>
-                Profession
-                <br />
-                Knight
-                <br />
-                Druid
-            </Col>
-            <Col xs = {2} sm={2} md={2}>
-                Level
-                <br />
-                5
-                <br />
-                23
-            </Col>
-            <Col xs = {2} sm={2} md={2}>
-                Hp
-                <br />
-                1000
-                <br />
-                500
-            </Col>
-            <Col xs = {2} sm={2} md={2}>
-                Mana
-                <br />
-                300
-                <br />
-                245
-            </Col>
-            <Col xs = {2} sm={2} md={2}>
-                Armor
-                <br />
-                23
-                <br />
-                33
-            </Col>
-            <Col xs = {2} sm={2} md={2}>
-                Items
-                <br />
-            </Col>
-            <br />
+                <Col xs = {2} sm={2} md={2}>   
+                    Profession
+                    <br />
+                    <HeroList/>
+                </Col>
+                <Col xs = {2} sm={2} md={2}>
+                    Level
+                    <br />
+                </Col>
+                <Col xs = {2} sm={2} md={2}>
+                    Hp
+                    <br/>
+                </Col>
+                <Col xs = {2} sm={2} md={2}>
+                    Mana
+                    <br />
+                </Col>
+                <Col xs = {2} sm={2} md={2}>
+                    Dmg
+                    <br />
+                </Col>
+                <Col xs = {2} sm={2} md={2}>
+                    Items
+                    <br />
+                </Col>
             </Row>
         </Grid>
       </div>
